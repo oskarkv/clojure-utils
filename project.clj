1cf81e8043a009e7a3fd366ba152116c456222b0
Oskar Kvist <oskar.kvist@gmail.com>
2024-07-09 06:49:03 +0200

    Delete project.clj

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject oskarkv/utils "0.1.0"
-  :url "https://github.com/oskarkv/clojure-utils"
-  :dependencies []
-  :source-paths ["src"]
-  :test-paths ["test"]
-  :license {:name "MIT License"}
-  :repositories [["clojars" {:url "https://clojars.org/repo"
-                             :sign-releases false}]])
